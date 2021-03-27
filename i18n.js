9c92e14a0d395eb2ab918907920aaa00d659189d
longvision <r.n.hori@gmail.com>
2021-03-27 13:07:07 -0300

    language default to us

--- i18n.js
+++ i18n.js
@@ -200,7 +200,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'pt',
+    lng: 'en',
     keySeparator: false, // we do not use keys in form messages.welcome
     interpolation: {
       escapeValue: false, // react already safes from xss
