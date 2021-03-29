4c9ddd22d730da988a1ab53a42e6f49ffef7d103
longvision <r.n.hori@gmail.com>
2021-03-29 19:31:25 -0300

    feat: image post done

--- src/store/models/index.js
+++ src/store/models/index.js
@@ -2,5 +2,6 @@ import {auth} from './auth';
 import {recipes} from './recipes';
 import {ingredients} from './ingredients';
 import {measures} from './measures';
+import {files} from './files';
 
-export const models = {auth, ingredients, recipes, measures};
+export const models = {auth, ingredients, recipes, measures, files};
