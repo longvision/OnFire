1d7595b1c2fc053fdcfce9202b20318167b13e14
longvision <r.n.hori@gmail.com>
2021-03-30 20:33:55 -0300

    feat: redux fixed for api updates

--- src/templates/RecipeListTemplate.js
+++ src/templates/RecipeListTemplate.js
@@ -11,28 +11,20 @@ import Loading from '../atoms/Loading';
 
 const RecipeListTemplate = ({AddIcon, navigation, recipes, iconName}) => {
   return (
-    <>
-      <Layout style={{height: '100%'}}>
-        <Layout
-          style={{
-            flex: 2,
-          }}>
-          <RecipeList
-            recipes={recipes}
-            rating={false}
-            navigation={navigation}
-            handleCamera={id => {
-              navigation.navigate('Camera', {
-                product_id: id,
-              });
-            }}
-            cta
-            assessoryLeft={iconName}
-            containerStyle={{width: '100%'}}
-          />
-        </Layout>
-      </Layout>
-    </>
+    <Layout style={{height: '100%'}}>
+      <RecipeList
+        recipes={recipes}
+        rating={false}
+        navigation={navigation}
+        handleCamera={id => {
+          navigation.navigate('Camera', {
+            product_id: id,
+          });
+        }}
+        cta
+        assessoryLeft={iconName}
+      />
+    </Layout>
   );
 };
 
