73907ac1bca03044fe9ef0890757e1508bff97b7
longvision <r.n.hori@gmail.com>
2021-04-02 22:10:43 -0300

    feat:release

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-<<<<<<< HEAD
     [
       'module:react-native-dotenv',
       {
@@ -13,17 +12,5 @@ module.exports = {
         allowUndefined: false,
       },
     ],
-=======
-    ['module:react-native-dotenv'],
-    // {
-    //   moduleName: '@env',
-    //   path: '.env',
-    //   blacklist: null,
-    //   whitelist: null,
-    //   safe: false,
-    //   allowUndefined: false,
-    // },
-    // ],
->>>>>>> ae2c67591a4ce6e213ef6b81c65d28bc866d154c
   ],
 };
